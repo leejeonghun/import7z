8276fd158ff34f3b28a6bebd6b0c28c145147351
leejeonghun <jeonghun@outlook.com>
2021-01-13 23:29:06 +0900

    Comply with PEP8

--- test/test_import.py
+++ test/test_import.py
@@ -7,11 +7,11 @@ import import7z
 class Unittest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         unittest.TestCase.__init__(self, *args, **kwargs)
-        import import7z
-        path7z = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test.7z')
+        cwd = os.path.dirname(os.path.abspath(__file__))
+        path7z = os.path.join(cwd, 'test.7z')
         sys.path.insert(0, path7z)
         sys.path_hooks.insert(0, import7z.importer7z)
-        sys.path_importer_cache.clear()        
+        sys.path_importer_cache.clear()
 
     def test_import_module(self):
         import module1
@@ -20,6 +20,7 @@ class Unittest(unittest.TestCase):
     def test_import_module_in_package(self):
         import pak.module2
         self.assertTrue(pak.module2.imported)
+
 
 if __name__ == "__main__":
     unittest.main()
